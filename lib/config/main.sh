4f3692247efad7267e2055ca5069850c6fdf3b32
lijunhao <l@x-cmd.com>
2022-07-06 13:56:56 +0800

    ...

--- lib/config/main.sh
+++ lib/config/main.sh
@@ -8,4 +8,6 @@ ___X_CMD_TMUX_BIN="${___X_CMD_TMUX_BIN:-tmux}"
 # Plugin management
 xrc:mod tmux/lib/config/plugin-util.sh
 
-xrc:mod tmux/lib/config/plugin-copy.sh  tmux/lib/config/plugin-scroll.sh
+xrc:mod     tmux/lib/config/plugin-copy.sh  \
+            tmux/lib/config/plugin-scroll.sh \
+            tmux/lib/config/plugin-theme-default.sh
