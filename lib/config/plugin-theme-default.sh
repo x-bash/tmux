42e1fcf0fc6c9076a7d3485b7aa575370f48f2e2
lijunhao <l@x-cmd.com>
2022-07-06 14:38:08 +0800

    ...

--- lib/config/plugin-theme-default.sh
+++ lib/config/plugin-theme-default.sh
@@ -1,10 +1,6 @@
 
 
-___x_cmd_tmux_set_color(){
-    : bg : color222 color111 color150 color140
-    $___X_CMD_TMUX_BIN set status-bg "$1"
-    # x $___X_CMD_TMUX_BIN set status-fg "$2"
-}
+$___X_CMD_TMUX_BIN set        status-interval 10
 
 $___X_CMD_TMUX_BIN setw -g    window-status-separator ' '
 $___X_CMD_TMUX_BIN setw -g    window-status-format "#I:#W "
@@ -18,11 +14,6 @@ $___X_CMD_TMUX_BIN set -g     status-left-style   "bg=orange"
 
 # $___X_CMD_TMUX_BIN set -g     status-left "#(date +%H:%M)  "
 
-$___X_CMD_TMUX_BIN set        status-interval 10
 # $___X_CMD_TMUX_BIN set -g     status-left "#(date +%T)  "
 $___X_CMD_TMUX_BIN set -g     status-right "#{host} #(date +%H:%M)"
-
-# Provide keby
-$___X_CMD_TMUX_BIN bind b     display-popup -E "${SHELL:-/bin/sh} $___X_CMD_ROOT_MOD/tmux/lib/config/popup.sh"
-
 
