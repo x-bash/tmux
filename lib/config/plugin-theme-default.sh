e1b520a5f63e01068175f393187c6247e74fb1fd
lijunhao <l@x-cmd.com>
2022-07-07 14:05:25 +0800

    ...

--- lib/config/plugin-theme-default.sh
+++ lib/config/plugin-theme-default.sh
@@ -15,5 +15,7 @@ $___X_CMD_TMUX_BIN set -g     status-left-style   "bg=orange"
 # $___X_CMD_TMUX_BIN set -g     status-left "#(date +%H:%M)  "
 
 # $___X_CMD_TMUX_BIN set -g     status-left "#(date +%T)  "
-$___X_CMD_TMUX_BIN set -g     status-right "#(sysctl -q -n vm.loadavg | cut -d' ' -f2-4) #{host} #(date +%H:%M)"
+
+___x_cmd_os_name_
+$___X_CMD_TMUX_BIN set -g     status-right "#(. $___X_CMD_ROOT_MOD/os/lib/loadavg; ___X_CMD_OS_NAME_=$___X_CMD_OS_NAME_ ___x_cmd_os_loadavg___get_fromos) | #{host} #(date +%H:%M)"
 
